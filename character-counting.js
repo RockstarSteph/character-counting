5c08ea2d448b0f43ca36fab0f30d2d5a0ac1a89a
RockstarSteph <stephaniechreifi@gmail.com>
2019-05-01 18:51:59 +0000

    my assignment on object

---
+++ character-counting.js
@@ -0,0 +1,23 @@
+
+var result = {};
+
+var ourStr = result["userinput"];
+
+
+/*function countLetters (str) {
+var ourStr = str.split(" ").join().toLowerCase();
+console.log(result["ourStr"]);*/
+
+//result.forEach(function(el, i)){
+//}
+
+//result["i"] = "number of counts"
+
+//for ( result in )
+
+//}
+
+//console.log (countLetters("lighthouse in the house"));
+// when we call the function it should return an object with each letter of str and their count
+
+// to add key to obj "person" person["phoneNumbers"] = ["514-333-9999", ""];
