5268a9005741ad739a1ab4758cea1e1cd759eb3d
RockstarSteph <42393050+RockstarSteph@users.noreply.github.com>
2019-05-01 15:00:39 -0400

    Update character-counting.js

--- character-counting.js
+++ character-counting.js
@@ -1,12 +1,21 @@
 
-var result = {};
-
-var ourStr = result["userinput"];
+//var ourStr = result["userinput"];
 
 
-/*function countLetters (str) {
-var ourStr = str.split(" ").join().toLowerCase();
-console.log(result["ourStr"]);*/
+function countLetters (str) {
+  var result = {};
+  var ourStr = str.split(" ").join("").split("");
+  //console.log(ourStr);
+  for (i=0; i<ourStr.length; i++){
+    var letter = ourStr[i];
+    // push this value into our result objec?
+
+    if (result[letter]) {
+      result[letter] += 1
+    } else {
+      result[letter] = 1;
+    }
+  }
 
 //result.forEach(function(el, i)){
 //}
@@ -14,10 +23,10 @@ console.log(result["ourStr"]);*/
 //result["i"] = "number of counts"
 
 //for ( result in )
+return result;
+}
 
-//}
-
-//console.log (countLetters("lighthouse in the house"));
+console.log(countLetters("lighthouse in the house"));
 // when we call the function it should return an object with each letter of str and their count
 
 // to add key to obj "person" person["phoneNumbers"] = ["514-333-9999", ""];
